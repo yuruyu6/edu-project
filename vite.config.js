34403016527d9aac681a046aaa558e1143190789
yuruyu6 <yuruyu6@gmail.com>
2023-02-25 14:26:15 +0200

    configure CI/CD

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   clearScreen: false,
-  /* logLevel: 'error', */
+  base: '/edu-project/',
   plugins: [react()],
   server: {
     port: 3030,
